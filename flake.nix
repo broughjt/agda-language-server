745f616801f299082fcf99d992094251263b779f
Jackson Brough <jacksontbrough@gmail.com>
2026-02-24 18:19:30 -0700

    Fix dev environment and tests

--- flake.nix
+++ flake.nix
@@ -11,19 +11,27 @@
       let
         pkgs = import nixpkgs { inherit system; };
         hpkgs = pkgs.haskellPackages;
+        hlib = pkgs.haskell.lib;
 
-        als = hpkgs.overrideCabal
-          (hpkgs.callCabal2nix "agda-language-server" ./. { })
-          (drv: {
-            buildInputs = (drv.buildInputs or [ ]) ++ [
-              pkgs.gmp
-              pkgs.ncurses
-              pkgs.zlib
-            ];
-          });
+        als =
+          hlib.overrideCabal
+            (hlib.addExtraLibraries
+              (hlib.doJailbreak (hpkgs.callCabal2nix "agda-language-server" ./. { }))
+              [ pkgs.gmp pkgs.ncurses pkgs.zlib ])
+            (_: {
+              doCheck = false;
+            });
 
-        alsChecks = hpkgs.overrideCabal als (drv: {
+        alsChecks = hlib.overrideCabal als (drv: {
           doCheck = true;
+          preCheck = (drv.preCheck or "") + ''
+            als_bin="$(find dist -type f -name als -perm -0100 | head -n1)"
+            if [ -z "$als_bin" ]; then
+              echo "Could not find built als executable under dist/" >&2
+              exit 1
+            fi
+            export PATH="$(dirname "$als_bin"):$PATH"
+          '';
         });
       in
       {
